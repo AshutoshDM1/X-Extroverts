74eb993fd7c1279cef5ef699134f2ea984734e51
AshutoshDM1 <downlodemaster1@gmail.com>
2026-08-18 20:48:54 +0530

    feat: add Step3Demographics component wrapper for demographic signup step

--- src/modules/SignUp/components/Step3Demographics.tsx
+++ src/modules/SignUp/components/Step3Demographics.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Step3Pronouns from './Step3Pronouns';
+
 export { Step3Pronouns as Step3Demographics } from './Step3Pronouns';
 export { Step3Pronouns } from './Step3Pronouns';
 export default Step3Pronouns;
